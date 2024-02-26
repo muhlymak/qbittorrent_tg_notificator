182cf1b4a987477ba110665f475ae3ee12f91416
muhlymak <muhlymak@icloud.com>
2024-02-26 22:09:38 +0700

    init project

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+import requests
+from dotenv import dotenv_values
+
+
+def telegram_notication(bot_token, chat_id, text_message) -> None:
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    data = {"chat_id": chat_id, "text": text_message}
+    requests.post(url, data=data)
+
+
+def main():
+    # Проверяем, есть ли аргументы командной строки
+    if len(sys.argv) < 2:
+        print("Usage: python script.py arg1 arg2 ...")
+        return
+
+    env_vars = dotenv_values(".env")
+
+    # Получаем переданные аргументы
+    args = sys.argv[1:]
+    text_message = f"Торрент {args[1]} скачан"
+    telegram_notication(
+        bot_token=env_vars["BOT_TOKEN"],
+        chat_id=env_vars["MESSAGE_TO"],
+        text_message=text_message,
+    )
+
+
+if __name__ == "__main__":
+    main()
