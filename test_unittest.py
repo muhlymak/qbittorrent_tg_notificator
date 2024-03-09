e6a8db9cc50e71248b5b42b1c3fdec6f1c800e8d
muhlymak <muhlymak@icloud.com>
2024-03-10 01:03:59 +0700

    git lens

--- test_unittest.py
+++ test_unittest.py
@@ -4,8 +4,7 @@ import requests
 
 
 class Test(TestCase):
-    def test_add_two_numbers(self):
-        self.assertTrue(summarize(a=5, b=6) == 11, "Should be 11")
+    
 
     def test(self):
         self.assertEqual(requests.get('https://google.com').status_code, 200)
