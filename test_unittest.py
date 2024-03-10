2bd1d66a6243b560c8cb61d5c106cbe0b7815f2a
muhlymak <muhlymak@icloud.com>
2024-03-10 11:02:14 +0700

    fix unittest

--- test_unittest.py
+++ test_unittest.py
@@ -4,11 +4,9 @@ import requests
 
 
 class Test(TestCase):
-    
-
     def test(self):
-        self.assertEqual(requests.get('https://google.com').status_code, 200)
+        self.assertEqual(requests.get("https://google.com").status_code, 200)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
